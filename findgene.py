9913ea8aaec2548ccdcd4cd517da0acd72936770
unknown <dieplanhoa1998@gmail.com>
2019-04-28 18:43:33 +0200

    find a list of potential sequence instead of only one sequence

--- findgene.py
+++ findgene.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
 
+
 with open("nissle_genom.txt","r") as file:
     gene_seq = file.read()
-
     def pre_gene_sequence(gene, length):
-        gene_index = gene_seq.find(gene)
-        if gene_index==-1:           
-            return "can not find the gene sequence"
-        else:
-            return gene_index, gene_seq[gene_index-1-length:gene_index-1]
-            
-    print (pre_gene_sequence("ggttttcttcaccagatccagcgccccccc",20))
+        i=0
+        potential_list =[]
+        while i < len(gene_seq):
+            gene_index = gene_seq.find(gene,i)
+            if gene_index==-1: 
+                if len(potential_list)==0:
+                    return "can not find the gene sequence"
+                else:
+                    return potential_list
+            else:
+                potential_list.append(gene_seq[gene_index-1-length:gene_index-1])
+                i = gene_index+1
+        return potential_list
+    print (pre_gene_sequence("tttcgccaaaatc",20))
+    
+
+
+
     
 
 
